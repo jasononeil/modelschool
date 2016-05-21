897c1f48103afbebef229ba7f90da6efd48da1c3
Jason O'Neil <jason.oneil@gmail.com>
2016-05-21 20:15:47 +0800

    Get `Home` to use a location too.

--- src/modelschool/core/model/Home.hx
+++ src/modelschool/core/model/Home.hx
@@ -7,9 +7,7 @@ import modelschool.core.model.*;
 class Home extends Object
 {
 	public var dbKey:String;
-	public var address:SString<255>;
-	public var state:SString<10>;
-	public var postcode:SString<5>;
+	public var address:BelongsTo<Location>;
 	public var phone:SString<20>;
 	public var fax:SString<20>;
 }
