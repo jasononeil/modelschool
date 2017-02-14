b7e9805b1ebbb97b4573362a9f47fc2fabeb94b5
Jason O'Neil <jason.oneil@gmail.com>
2017-02-14 21:50:14 +1100

    Add CoreApi build that compiles everything.
    Also acts as an "import everything in this lib/package" file

---
+++ src/modelschool/CoreApi.hx
@@ -0,0 +1,36 @@
+package modelschool;
+
+class CoreApi {}
+
+typedef ClassListHistory = modelschool.core.model.ClassListHistory.ClassListHistory;
+typedef TimetableSet = modelschool.core.model.ClassListHistory.TimetableSet;
+typedef StudentEnrolmentHistory = modelschool.core.model.ClassListHistory.StudentEnrolmentHistory;
+typedef TeacherEnrolmentHistory = modelschool.core.model.ClassListHistory.TeacherEnrolmentHistory;
+typedef HistoricClassList = modelschool.core.model.ClassListHistory.HistoricClassList;
+typedef HistoricTimetable = modelschool.core.model.ClassListHistory.HistoricTimetable;
+typedef ClassTime = modelschool.core.model.ClassTime;
+typedef ContactDetails = modelschool.core.model.ContactDetails.ContactDetails;
+typedef ContactDetail = modelschool.core.model.ContactDetails.ContactDetail;
+typedef ContactDetailTools = modelschool.core.model.ContactDetails.ContactDetailTools;
+typedef Department = modelschool.core.model.Department;
+typedef District = modelschool.core.model.District;
+typedef Family = modelschool.core.model.Family;
+typedef Gender = modelschool.core.model.Gender;
+typedef Home = modelschool.core.model.Home;
+typedef Location = modelschool.core.model.Location;
+typedef Parent = modelschool.core.model.Parent;
+typedef Period = modelschool.core.model.Period;
+typedef Person = modelschool.core.model.Person;
+typedef RollGroup = modelschool.core.model.RollGroup;
+typedef Room = modelschool.core.model.Room;
+typedef School = modelschool.core.model.School;
+typedef SchoolClass = modelschool.core.model.SchoolClass;
+typedef SchoolHouse = modelschool.core.model.SchoolHouse;
+typedef StaffMember = modelschool.core.model.StaffMember;
+typedef StaffMemberProfile = modelschool.core.model.StaffMemberProfile;
+typedef Student = modelschool.core.model.Student;
+typedef StudentPhoto = modelschool.core.model.StudentPhoto;
+typedef StudentProfile = modelschool.core.model.StudentProfile.StudentProfile;
+typedef ResidentStatus = modelschool.core.model.StudentProfile.ResidentStatus;
+typedef IndigenousStatus = modelschool.core.model.StudentProfile.IndigenousStatus;
+typedef Subject = modelschool.core.model.Subject;
