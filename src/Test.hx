c4ea22eee63b6930cbe2a40c75379f9d294179f9
Jason O'Neil <jason.oneil@gmail.com>
2017-02-28 21:03:30 +1100

    Add very basic test for reading from a MySQL DB.
    Also add the visual studio code config for Haxe.

---
+++ src/Test.hx
@@ -0,0 +1,18 @@
+import sys.db.*;
+import haxe.Utf8;
+
+class Test {
+	static function main() {
+		Manager.cnx = Mysql.connect({
+			host: 'db_1',
+			user: 'root',
+			pass: 'root',
+			database: 'modelschool_test_maze1'
+		});
+		var studentResults = Manager.cnx.request('SELECT first_name, pref_name, surname FROM ST WHERE status="FULL"');
+		for (s in studentResults.results()) {
+			var s = Utf8.encode(Std.string(s));
+			Sys.println('$s<br>');
+		}
+	}
+}
